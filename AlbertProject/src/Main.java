a4558987c1e6385f08c5e18c20c779dfe7823913
KiraYagamov <105204832+KiraYagamov@users.noreply.github.com>
2024-01-22 19:00:24 +0300

    commit

--- AlbertProject/src/Main.java
+++ AlbertProject/src/Main.java
@@ -1,10 +1,43 @@
-import Test.Person;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Person person = new Person("Gdfgdfg");
-        person.setName();
-        person.getData();
-        person.sayHello("");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите число: ");
+        String line = scanner.nextLine();
+        int count = Integer.parseInt(line);
+
+        int count3 = 0;
+        int count4 = 0;
+        int num = 1;
+        while(count3 < count && count4 < count){
+            if(num % 3 == 0 && num % 4 == 0){
+                count3++;
+                count4++;
+                System.out.println("Число " + num + " - делится на и 3, и на 4. " + "Номер тройки - " + count3 + ". Номер четверки - " + count4);
+            }
+            else if(num % 3 == 0){
+                count3++;
+                System.out.println("Число " + num + " - делится на 3. " + "Номер тройки " + count3);
+            }
+            else if(num % 4 == 0){
+                count4++;
+                System.out.println("Число " + num + " - делится на 4. " + "Номер четверки " + count4);
+            }
+            else{
+                System.out.println("Число " + num + " - не подходит.");
+            }
+            num++;
+        }
+
+        if(count3 > count4){
+            System.out.println("Победила тройка");
+        }
+        else if(count4 > count3){
+            System.out.println("Победила четверка");
+        }
+        else{
+            System.out.println("Победила дружба");
+        }
     }
 }
